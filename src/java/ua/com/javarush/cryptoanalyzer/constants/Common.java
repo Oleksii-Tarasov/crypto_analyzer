a0ea02a5b4c99d9ded0cc532c086c24fad21eb77
Oleksii-Tarasov <alekseytarasov85@gmail.com>
2022-06-03 13:15:40 +0300

    Divided the class of constants into separate classes.

---
+++ src/java/ua/com/javarush/cryptoanalyzer/constants/Common.java
@@ -0,0 +1,6 @@
+package ua.com.javarush.cryptoanalyzer.constants;
+
+public class Common {
+    public static final String DOUBLE_SLASH = "\\";
+    public static final String DASH = " - ";
+}
---
+++ src/java/ua/com/javarush/cryptoanalyzer/constants/ConsoleOptions.java
@@ -0,0 +1,9 @@
+package ua.com.javarush.cryptoanalyzer.constants;
+
+public class ConsoleOptions {
+    public static final String ENTER_OPTIONS = "Choose option: ";
+    public static final String ENTER_INPUT_FILE = "Choose input file: ";
+    public static final String ENTER_OUTPUT_FILE_NAME = "Choose output file name: ";
+    public static final String ENTER_OUTPUT_FOLDER = "Choose output folder: ";
+    public static final String ENTER_ENCRYPTION_KEY = "Enter the encryption key: ";
+}
